58695f840dc1d112b7f7884328eb26d381dac85b
Juho Seppnane <juho.seppanen@evli.com>
2018-08-04 13:11:28 +0300

    Update

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # News scraper 
 
+
 go run main.go
 
 
+
+Testing out concurrency in go:
+![Concurrency](https://github.com/azor0/News/assets/Concurrent.png)
+
+![Linear](https://github.com/azor0/News/assets/Linear.png)
--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 	"html/template"
 	"encoding/xml"
 	"io/ioutil"
+	"sync"
 )
+
+var wg sync.WaitGroup
+var client http.Client
+
 // type Location struct {
 // 	Loc string `xml:"loc"`
 // }
@@ -17,8 +22,6 @@ import (
 // 	httpsFix := l.Loc[:4] + "s" + l.Loc[4:]
 // 	return httpsFix
 // }
-
-
 
 type NewsMap struct {
 	Keyword string
@@ -43,15 +46,29 @@ type News struct {
 
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
+	fmt.Fprintf(w, "<h1>Go to /news</h1>")
 }
-// func makeRequest(url string)
 
-func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-	var s Sitemapindex
+func newsRoutine(c chan News, Location string){
+	defer wg.Done()
+
 	var n News
+	url := Location[:4] + "s" + Location[4:]
+	req, _ := http.NewRequest("GET", url, nil)
+	cookie1 := http.Cookie{Name: "wp_gdpr", Value: "1|1" }
+	req.AddCookie(&cookie1)
+	response, _ := client.Do(req)
 
-	client := &http.Client{}
+	bytes, _ := ioutil.ReadAll(response.Body)
+	xml.Unmarshal(bytes, &n)
+	response.Body.Close()
+
+	c <- n
+}
+
+func newsHandler(w http.ResponseWriter, r *http.Request) {
+	var s Sitemapindex
+
 	req, _ := http.NewRequest("GET", "https://www.washingtonpost.com/news-sitemap-index.xml?", nil)
 	cookie1 := http.Cookie{Name: "wp_gdpr", Value: "1|1" }
     req.AddCookie(&cookie1)
@@ -60,35 +77,28 @@ func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	bytes, _ := ioutil.ReadAll(res.Body)
 	xml.Unmarshal(bytes, &s)
 	news_map := make(map[string]NewsMap) // key is string and values are NewsMap values
-
+	res.Body.Close()
+	queue := make(chan News, 30)
 
 	for _, Location := range s.Locations {
-		testing := fmt.Sprintf("%s s %s", Location[:4], Location[4:])
-		url := Location[:4] + "s" + Location[4:]
-		req, _ := http.NewRequest("GET", url, nil)
-		cookie1 := http.Cookie{Name: "wp_gdpr", Value: "1|1" }
-		req.AddCookie(&cookie1)
-		fmt.Println("testing:", testing)
-		test, _ := client.Do(req)
-
-		bytes, _ := ioutil.ReadAll(test.Body)
-		string_body := string(bytes)
-		fmt.Println(string_body)
-		xml.Unmarshal(bytes, &n)
-		for idx, _ := range n.Keywords {
-			fmt.Println("Heere2")
-			fmt.Println(n.Titles[idx], idx)
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		wg.Add(1)
+		go newsRoutine(queue, Location)
+	}
+	wg.Wait()
+	close(queue)
+	for elem := range queue { 
+		for idx, _ := range elem.Keywords {
+			news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
 		}
 	}
 
-	p := NewsAggPage{Title: "NEWS", News: news_map}
+	p := NewsAggPage{Title: "NEWS WEBSITE", News: news_map}
     t, _ := template.ParseFiles("template.html")
     t.Execute(w, p)
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/agg/", newsAggHandler)
+	http.HandleFunc("/news/", newsHandler)
 	http.ListenAndServe(":8000", nil) 
 }
